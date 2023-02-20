afed1fc111f165f4672bf7b6041f22ecf6c5663f
Paula <paula_baranano1@baylor.edu>
2023-02-20 14:14:42 -0600

    hmw

---
+++ PersonClass.py
@@ -0,0 +1,23 @@
+
+class Person:
+    def __init__(self, name, address, phone):
+        self.__name = name
+        self.__address = address
+        self.phone = phone
+    
+    def print_person(self):
+        print('Name', self.__name, 'Address:', self.__address, 'Phone:', self.__phone)
+
+class Customer(Person):
+    def __init__(self, name, address, phone, cust_number, mailing):
+        Person.__init__(self, name, address, phone)
+    
+        self.__cust_number = cust_number
+        self.__mailing = mailing 
+    
+    def print_person(self):
+        print('Name:', self.__name)
+        print('Address:', self.__address)
+        print('Phone:', self.__phone)
+        print('Customer number:', self.__cust_number)
+        print('Mailing:', self.__mailing)
